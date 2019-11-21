7d5760f70a9b59d30be41f5a93f04c18d260409f
Yi Zu <zuyi@bu.edu>
2019-11-21 13:08:02 +0100

    lesson 12

---
+++ http-app/src/App.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+//Only use try catch block when you have to do something as result of failure
+//Otherwise use interceptor
+//BUG: EXPECTER ERRORS ARE NOT BEING CAUGHT
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  //Unexpected error happens
+  if (!expectedError) {
+    console.log("Logging error", error);
+    alert("An unexpected error occured");
+  }
+
+  return Promise.reject(error);
+});
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    // pending > resolved (success) OR rejected (failure)
+    // await word gets result of promise
+    //need to put async in method signature in componentDidMount
+    const { data: posts } = await axios.get(apiEndpoint);
+
+    this.setState({ posts });
+  }
+
+  //property for a function
+  handleAdd = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
+
+    console.log(post);
+  };
+
+  handleUpdate = async post => {
+    post.title = "UPDATED";
+
+    //Put to update entire post
+    await axios.put("s" + apiEndpoint + "/" + post.id);
+
+    //Clone posts array
+    const posts = [...this.state.posts];
+
+    //Find index of post in array to update
+    const index = posts.indexOf(post);
+
+    //Create new object in post array at specific index
+    posts[index] = { ...post };
+    this.setState({ posts });
+  };
+
+  handleDelete = async post => {
+    const originalPosts = this.state.posts;
+
+    //Get object with all posts except the one that you deleted
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
+
+    //Optimistic update
+    //1. Keep reference of original state
+    //2. Update UI before call to server
+    //3. Wrap call to server in try catch block
+
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        alert("This post has already been deleted");
+      this.setState({ posts: originalPosts });
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
